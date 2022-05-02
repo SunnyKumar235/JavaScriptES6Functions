60faeaf1dacc0dc6037e6122abe2a8fbbc18c6f3
SunnyKumar <sunny@staytidy.co>
2022-05-02 19:34:58 +0500

    seperate the data from other files

---
+++ sampleData.js
@@ -0,0 +1,32 @@
+const characters =[
+    {
+        name : "Sunny",
+        height: 5.7,
+        mass : 62,
+        eye_color: "blue",
+        gender : "Male" 
+    },
+
+    {
+        name : "SpiderMan",
+        height: 5.8,
+        mass : 72,
+        eye_color: "blue",
+        gender : "Male" 
+    },
+    {
+        name : "WonderWomen",
+        height: 5.7,
+        mass : 101,
+        eye_color: "blue",
+        gender : "Women" 
+    },
+    {
+        name : "Iron Man",
+        height: 5.7,
+        mass : 120,
+        eye_color: "blue",
+        gender : "Man" 
+    },
+]
+module.exports = characters;
